7d48a66ee73218b5427ef7f5217c758895b54037
django <2766184595@qq.com>
2023-10-03 15:15:51 +0800

    hello

---
+++ hello.py
@@ -0,0 +1,14 @@
+import time
+
+def start_countdown(n):
+    for i in range(n, 0, -1):
+        print(i)
+        time.sleep(1)
+    print('发射！')
+
+def start_rocket():
+    print('飞船正在准备起飞......')
+    start_countdown(3)
+    print('飞船成功起飞！')
+
+start_rocket()
